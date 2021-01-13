2cbc28b87a8dd0100c216c4b203e26c911d80e6e
Luis Miguel Del Gallego <luis.delgallego@avaldigitallabs.com>
2021-01-12 22:37:54 -0500

    Use Qr Cam

---
+++ lib/models/scan_models.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+import 'package:meta/meta.dart';
+
+ScanModel scanModelFromJson(String str) => ScanModel.fromJson(json.decode(str));
+
+String scanModelToJson(ScanModel data) => json.encode(data.toJson());
+
+class ScanModel {
+  ScanModel({
+    this.id,
+    this.type,
+    @required this.value,
+  }) {
+    if (this.type.contains('http')) {
+      this.type = 'http';
+    } else {
+      this.type = 'geo';
+    }
+  }
+  int id;
+  String type;
+  String value;
+
+  factory ScanModel.fromJson(Map<String, dynamic> json) => ScanModel(
+        id: json["id"],
+        type: json["type"],
+        value: json["value"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "type": type,
+        "value": value,
+      };
+}
