a5238967464fdee8980a7ef2dadf5ec5fee91494
Luis Miguel Del Gallego <luis.delgallego@avaldigitallabs.com>
2021-01-18 09:48:37 -0500

    install google maps

--- lib/pages/map_history_page.dart
+++ lib/pages/map_history_page.dart
@@ -1,17 +1,36 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:qr_reader/providers/db_provider.dart';
 
-class MapHistoryPage extends StatelessWidget {
+class MapHistoryPage extends StatefulWidget {
+  @override
+  _MapHistoryPageState createState() => _MapHistoryPageState();
+}
+
+class _MapHistoryPageState extends State<MapHistoryPage> {
+  Completer<GoogleMapController> _controller = Completer();
+
   @override
   Widget build(BuildContext context) {
+    final CameraPosition puntoInicial = CameraPosition(
+      target: LatLng(37.42796133580664, -122.085749655962),
+      zoom: 14.4746,
+    );
+
     final ScanModel scan = ModalRoute.of(context).settings.arguments;
 
     return Scaffold(
       appBar: AppBar(
         title: Text('MAPA'),
       ),
-      body: Center(
-        child: Text(scan.value),
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: puntoInicial,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
       ),
     );
   }
